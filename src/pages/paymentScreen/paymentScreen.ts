6fd4e3eea08a94d41710182a99054401d92f0b31
VTSundar <sundar@vthinkdeveloper.com>
2017-03-02 18:27:58 +0530

    PAyment Screen Ui Issue Fixed

--- src/pages/paymentScreen/paymentScreen.ts
+++ src/pages/paymentScreen/paymentScreen.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'payment-screen',
   templateUrl: 'paymentScreen.html'
 })
 export class PaymentPage {
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+    public platFormType : any;
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController,public platform: Platform) {
+  if (this.platform.is('android')) {
+     this.platFormType = 'Mobile';
+   }
+   else if(this.platform.is('ios')){
+     this.platFormType = 'Ios';
+   }
+   else{
+        this.platFormType = 'Windows';
+   }
+   console.log(this.platFormType);
   }
 }
